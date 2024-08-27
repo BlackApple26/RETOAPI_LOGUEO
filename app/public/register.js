e0df46c7a9a60b1041718150d7bdb4f6ae3ee511
Paula Osorio <paulaosorio726@gmail.com>
2024-08-27 05:19:37 -0500

    Cambio de dirección fetch

--- app/public/register.js
+++ app/public/register.js
@@ -3,7 +3,7 @@ const mensajeError = document.getElementsByClassName("error")[0];
 document.getElementById("register-form").addEventListener("submit",async(e)=>{
   e.preventDefault();
   console.log(e.target.children.user.value)
-  const res = await fetch("https://retoapi-logueo.onrender.com",{
+  const res = await fetch("https://retoapi-logueo.onrender.com/register",{
     method:"POST",
     headers:{
       "Content-Type" : "application/json"
